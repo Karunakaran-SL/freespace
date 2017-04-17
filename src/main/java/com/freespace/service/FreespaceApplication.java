f3333ceda2b49ad695d5ca3cd51605de41bac284
Subash Chandrabose, Karunakaran <karunakaran.subashchandrabose@astrazeneca.com>
2017-04-17 11:18:57 +0530

    Init

---
+++ src/main/java/com/freespace/service/FreespaceApplication.java
@@ -0,0 +1,12 @@
+package com.freespace.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreespaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreespaceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/freespace/service/FreespaceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.freespace.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FreespaceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
